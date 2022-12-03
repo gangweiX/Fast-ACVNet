8ef34e69eb9394f54698e444c0aea3b9a9445043
Gangwei Xu <34023118+gangweiX@users.noreply.github.com>
2022-12-03 20:33:39 +0800

    Delete datasets directory

--- datasets/__init__.py
+++
@@ -1,9 +0,0 @@
-from .kitti_dataset_1215_augmentation import KITTIDataset
-from .sceneflow_dataset_augmentation import SceneFlowDatset
-from .middlebury_data_our import MiddleburyStereoDataset
-
-__datasets__ = {
-    "sceneflow": SceneFlowDatset,
-    "kitti": KITTIDataset,
-    "middlebury":MiddleburyStereoDataset
-}
